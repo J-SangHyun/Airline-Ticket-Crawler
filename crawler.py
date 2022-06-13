9980c0dd26e67887900b080b8f4a8af3e0f6ec4c
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2022-06-13 21:16:36 +0900

    crawler changed to google flight

--- crawler.py
+++ crawler.py
@@ -3,80 +3,139 @@ import time
 from constant import city2code
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-BASE_URL = 'https://flight.naver.com/flights/international/'
-DETAILED_URL = BASE_URL + '%s-%s-%s/%s-%s-%s?adult=1&isDirect=true&fareType=Y&selectType=concurrent'
-DELAY = 0.1
+BASE_URL = 'https://www.google.com/travel/flights?tfs=CBwQARoaagwIAhIIL20vMGhzcWYSCjIwMjItMDYtMjga' \
+           'GhIKMjAyMi0wNy0wMnIMCAISCC9tLzBoc3FmcAGCAQsI____________AUABSAGYAQE'
+XPATH_DEST = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/' \
+             'div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input'
+XPATH_DEST2 = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/' \
+              'div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input'
+XPATH_CALENDAR = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/' \
+                 'div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input'
+XPATH_RESET = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/' \
+              'div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[2]/div[2]/button'
+XPATH_ORIGIN_DATE = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/' \
+                    'div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/input'
+XPATH_DEST_DATE = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/' \
+                  'div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/input'
+XPATH_SUBMIT = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/' \
+               'div[2]/div[2]/div/div/div[2]/div/div[3]/div[3]/div/button'
+XPATH_LOADING = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[2]/div[2]/div[1]'
+XPATH_DIRECT = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div/div[4]/' \
+               'div/div/div[2]/div[1]/div/div[1]/span/button'
+XPATH_ONLY_DIRECT = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div/' \
+                    'div[4]/div/div[2]/div[3]/div/div[1]/section/div[2]/div/div[2]/div/input'
+XPATH_CLOSE_DIRECT = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div/div[4]/' \
+                     'div/div[2]/div[3]/div/div[1]/section/div[1]/button'
+XPATH_SORT = '//button[contains(., "정렬기준:")]'
+XPATH_SORT_PRICE = '//li[contains(., "요금")]'
+XPATH_LIST = '/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[2]/ul'
+DELAY = 0.4
+DELAY_LOAD = 2
 
 
-def get_url(port1, port2, date1, date2):
-    return DETAILED_URL % (port1, port2, date1, port2, port1, date2)
-
-
-def get_price(driver, url, airlines):
-    driver.get(url)
+def get_price(driver, city, date1, date2, airlines):
+    driver.get(BASE_URL)
     current_prices = [-1 for _ in range(len(airlines))]
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, XPATH_DEST)))
+    # click destination
+    time.sleep(DELAY_LOAD)
+    driver.find_element(By.XPATH, XPATH_DEST).click()
+    time.sleep(DELAY)
+
+    # input destination
+    dest2 = driver.find_element(By.XPATH, XPATH_DEST2)
+    dest2.send_keys(city2code[city])
+    time.sleep(DELAY)
+    dest2.send_keys(Keys.ENTER)
+    time.sleep(DELAY)
+
+    # open calendar
+    driver.find_element(By.XPATH, XPATH_CALENDAR).click()
+    time.sleep(DELAY)
+
+    # reset calendar
+    driver.find_element(By.XPATH, XPATH_RESET).click()
+    time.sleep(DELAY)
+
+    # setting origin->dest date
+    origin = driver.find_element(By.XPATH, XPATH_ORIGIN_DATE)
+    origin.click()
+    time.sleep(DELAY)
+    origin.send_keys(Keys.BACKSPACE)
+    time.sleep(DELAY)
+    origin.send_keys(date1)
+    time.sleep(DELAY)
+    origin.send_keys(Keys.ENTER)
+    time.sleep(DELAY)
+
+    # setting dest->origin date
+    dest = driver.find_element(By.XPATH, XPATH_DEST_DATE)
+    dest.click()
+    time.sleep(DELAY)
+    dest.send_keys(Keys.BACKSPACE)
+    time.sleep(DELAY)
+    dest.send_keys(date2)
+    time.sleep(DELAY)
+    dest.send_keys(Keys.ENTER)
+    time.sleep(DELAY)
+
+    # submit
+    driver.find_element(By.XPATH, XPATH_SUBMIT).click()
+    time.sleep(DELAY)
+
+    # choose direct
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, XPATH_LOADING)))
+    driver.find_element(By.XPATH, XPATH_DIRECT).click()
+    time.sleep(DELAY)
+    only_direct = driver.find_element(By.XPATH, XPATH_ONLY_DIRECT)
     try:
-        WebDriverWait(driver, 40).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'inlineFilter_inner__1DuQ7'))
-        )
+        WebDriverWait(driver, DELAY).until(EC.element_to_be_clickable(only_direct))
+    except:
+        return current_prices
     finally:
-        if driver.find_elements(By.CLASS_NAME, 'concurrent_ConcurrentItemContainer__2lQVG'):
-            driver.find_element(By.XPATH,
-                                '//button[@class="inlineFilter_Tag__97qqq"][normalize-space()="요금조건"]').click()
-            time.sleep(DELAY)
-            driver.find_element(By.XPATH, '//button[@class="checkbox"][normalize-space()="성인"]').click()
-            time.sleep(DELAY)
-            driver.find_element(By.XPATH, f'//button[@class="submit"]').click()
-            time.sleep(DELAY)
+        only_direct.click()
+        time.sleep(DELAY)
+        driver.find_element(By.XPATH, XPATH_CLOSE_DIRECT).click()
+        time.sleep(DELAY_LOAD)
 
-            for i in range(len(airlines)):
-                airline = airlines[i]
-                driver.find_element(By.XPATH,
-                                    '//button[@class="inlineFilter_Tag__97qqq"][contains(., "항공사")]').click()
-                time.sleep(DELAY)
-                driver.find_element(By.XPATH, '//button[@class="checkbox as_all"][normalize-space()="전체"]').click()
-                time.sleep(DELAY)
-                if not driver.find_elements(By.XPATH, f'//button[@class="checkbox"][normalize-space()="{airline}"]'):
-                    current_prices[i] = -1
-                    driver.find_element(By.XPATH, f'//button[@class="submit"]').click()
-                    time.sleep(DELAY)
-                    continue
-                driver.find_element(By.XPATH, f'//button[@class="checkbox"][normalize-space()="{airline}"]').click()
-                time.sleep(DELAY)
-                driver.find_element(By.XPATH, f'//button[@class="submit"]').click()
-                time.sleep(DELAY)
+        # sort
+        if not driver.find_elements(By.XPATH, XPATH_LIST):
+            return current_prices
+        driver.find_element(By.XPATH, XPATH_SORT).click()
+        time.sleep(DELAY)
+        driver.find_element(By.XPATH, XPATH_SORT_PRICE).click()
+        time.sleep(DELAY_LOAD)
 
-                tickets = driver.find_elements(By.CLASS_NAME, 'concurrent_ConcurrentItemContainer__2lQVG')
-                for ticket in tickets:
-                    if ticket.find_elements(By.CLASS_NAME, 'concurrent_RoundSameAL__1Y3j3'):
-                        current_price = int(ticket.find_element(By.CLASS_NAME, 'item_num__3R0Vz').text.replace(',', ''))
-                        current_prices[i] = current_price
-                        break
-                time.sleep(DELAY)
-    return current_prices
+        # get tickets
+        if not driver.find_elements(By.XPATH, XPATH_LIST):
+            return current_prices
+        ticket_list = driver.find_element(By.XPATH, XPATH_LIST)
+        tickets = ticket_list.find_elements(By.CLASS_NAME, 'JjMzFd')
+        for ticket in tickets:
+            name_module = ticket.find_element(By.CLASS_NAME, 'TQqf0e')
+            airline_name = name_module.find_elements(By.TAG_NAME, 'span')[0].text
+            if airline_name == '함께 예약된 다구간 항공권':
+                airline_name = name_module.find_elements(By.TAG_NAME, 'span')[-1].text
+            price_module = ticket.find_element(By.CLASS_NAME, 'U3gSDe')
+            ticket_price = int(price_module.find_element(By.TAG_NAME, 'span').text.replace('₩', '').replace(',', ''))
 
-
-def get_minimum_price(driver, city1, city2, date1, date2, airlines):
-    minimum_prices = [-1 for _ in range(len(airlines))]
-    for port1 in city2code[city1]:
-        for port2 in city2code[city2]:
-            url = get_url(port1, port2, date1, date2)
-            _prices = get_price(driver, url, airlines)
-            time.sleep(2)
-            for i in range(len(airlines)):
-                if minimum_prices[i] == -1 or _prices[i] < minimum_prices[i]:
-                    minimum_prices[i] = _prices[i]
-    return minimum_prices
+            if airline_name in airlines:
+                idx = airlines.index(airline_name)
+                if current_prices[idx] == -1 or ticket_price < current_prices[idx]:
+                    current_prices[idx] = ticket_price
+        print(city, date1, date2, current_prices)
+        return current_prices
 
 
 if __name__ == '__main__':
     CHROME_DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    price = get_minimum_price(CHROME_DRIVER, '서울', '도쿄', '20220608', '20220611', ['대한항공', '아시아나항공'])
+    price = get_price(CHROME_DRIVER, '뉴욕', '07-16', '07-22', ['대한항공', '아시아나'])
     #price = get_minimum_price(CHROME_DRIVER, '서울', '도쿄', 20220801, 20220822, ['대한항공'])
     #price = get_minimum_price(CHROME_DRIVER, '서울', '도쿄', 20220622, 20220623, ['대한항공'])
     #price = get_minimum_price(CHROME_DRIVER, '서울', '도쿄', 20220801, 20220822, ['필리핀항공'])
--- main.py
+++ main.py
@@ -1,60 +1,120 @@
 # -*- coding: utf-8 -*-
+import tkinter as tk
+import tkinter.ttk as ttk
 from tqdm import tqdm
 from datetime import date, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from openpyxl import Workbook
-from crawler import get_minimum_price
-from constant import cityEurope, cityAmerica
+from crawler import get_price
+from constant import city2code, cityEurope, cityAmerica
 
 
 if __name__ == '__main__':
-    CHROME_DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    workbook = Workbook()
-    today = date.today()
+    main = tk.Tk()
+    main.title('Airline Ticket Crawler')
+    main.geometry('640x420')
+    main.resizable(False, False)
+
     day_list = ['일', '월', '화', '수', '목', '금', '토']
-    #city_list = ['도쿄', '괌', '오사카', '뉴욕', '런던']
-    #airline_list = ['대한한공', '제주항공', '아시아나항공', '진에어']
-    city_list = ['도쿄']
-    airline_list = ['대한항공', '아시아나항공']
+    total_airline_list = \
+        ['뉴질랜드항공', '대한항공', '델타항공', '방콕항공', '베트남항공', '싱가포르항공', '아시아나', '에어부산', '에어서울',
+         '유나이티드항공', '일본항공', '전일본공수', '제주항공', '중화항공', '진에어', '캐세이패시픽항공', '티웨이항공',
+         '필리핀항공', 'MIAT 몽골항공', 'THAI']
 
-    sheet = workbook.active
-    sheet.title = today.strftime('%Y%m%d')
-    sheet.cell(row=2, column=2, value='출발일')
-    workbook.save(f'./{today.strftime("%Y%m%d")}.xlsx')
+    airline_checked = [tk.IntVar() for _ in range(len(total_airline_list))]
+    airline_buttons = [tk.Checkbutton(main, text=total_airline_list[i], variable=airline_checked[i])
+                       for i in range(len(total_airline_list))]
+    for i in range(len(airline_buttons)):
+        airline_buttons[i].place(x=10, y=20*i+10)
 
-    for i in range(len(city_list)):
-        sheet.cell(column=3+i*len(airline_list), row=1, value=city_list[i])
-        sheet.merge_cells(start_column=3+i*len(airline_list), start_row=1,
-                          end_column=2+(i+1)*len(airline_list), end_row=1)
-        for j in range(len(airline_list)):
-            sheet.cell(column=3+i*len(airline_list)+j, row=2, value=airline_list[j])
-    workbook.save(f'./{today.strftime("%Y%m%d")}.xlsx')
+    cities = list(city2code.keys())
+    city_checked = [tk.IntVar() for _ in range(len(cities))]
+    city_buttons = [tk.Checkbutton(main, text=cities[i], variable=city_checked[i])
+                    for i in range(len(cities))]
+    for i in range(len(city_buttons)):
+        city_buttons[i].place(x=200, y=20*i+10)
 
-    for tqdm_i in tqdm(range(len(city_list) * 3 * 7)):
-        i = tqdm_i // (3 * 7)
-        j = (tqdm_i % (3 * 7)) // 7
-        k = tqdm_i % 7
+    n_dates = 8
+    dates_from = [tk.Entry(main, width=9) for _ in range(n_dates)]
+    dates_from_t = [tk.Label(main, text='부터') for _ in range(n_dates)]
+    dates_period = [tk.Entry(main, width=2) for _ in range(n_dates)]
+    dates_period_t = [tk.Label(main, text='일간 검색') for _ in range(n_dates)]
+    for i in range(n_dates):
+        y = (i+1)*30
+        dates_from[i].place(x=380, y=y)
+        dates_from_t[i].place(x=450, y=y)
+        dates_period[i].place(x=500, y=y)
+        dates_period_t[i].place(x=520, y=y)
 
-        city = city_list[i]
-        period = 6 if (city in cityEurope or city in cityAmerica) else 3
-        date1 = today + timedelta(days=28*j+k)
-        date2 = date1 + timedelta(days=period)
-        prices = get_minimum_price(CHROME_DRIVER, '서울', city,
-                                   date1.strftime("%Y%m%d"), date2.strftime("%Y%m%d"), airline_list)
-        cell_row = 3 + 7*j + k
-        sheet.cell(column=2, row=cell_row, value=f'{int(date1.strftime("%Y"))}년 '
-                                                 f'{int(date1.strftime("%m"))}월 '
-                                                 f'{int(date1.strftime("%d"))}일 '
-                                                 f'{day_list[int(date1.strftime("%w"))]}요일')
+    progress = ttk.Progressbar(main, length=200)
+    progress.place(x=380, y=360)
 
-        for l in range(len(airline_list)):
-            cell_column = 3 + i * len(airline_list) + l
-            price = prices[l]
-            if price != -1:
-                sheet.cell(column=cell_column, row=cell_row, value=prices[l])
-        workbook.save(f'./{today.strftime("%Y%m%d")}.xlsx')
+    def crawling():
+        CHROME = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        today = date.today()
+        workbook = Workbook()
 
-    workbook.save(f'./{today.strftime("%Y%m%d")}.xlsx')
-    CHROME_DRIVER.quit()
+        sheet = workbook.active
+        sheet.title = today.strftime('%Y%m%d')
+        sheet.cell(row=2, column=2, value='출발일')
+        workbook.save(f'{today.strftime("%Y%m%d")}.xlsx')
+
+        airline_list, city_list = [], []
+        for i in range(len(airline_checked)):
+            if airline_checked[i].get() == 1:
+                airline_list.append(total_airline_list[i])
+        for i in range(len(cities)):
+            if city_checked[i].get() == 1:
+                city_list.append(cities[i])
+
+        for i in range(len(city_list)):
+            sheet.cell(column=3 + i * len(airline_list), row=1, value=city_list[i])
+            sheet.merge_cells(start_column=3 + i * len(airline_list), start_row=1,
+                              end_column=2 + (i + 1) * len(airline_list), end_row=1)
+            for j in range(len(airline_list)):
+                sheet.cell(column=3 + i * len(airline_list) + j, row=2, value=airline_list[j])
+        workbook.save(f'{today.strftime("%Y%m%d")}.xlsx')
+
+        total_dates = 0
+        dates = []
+        for i in range(n_dates):
+            if dates_from[i].get() != '' and dates_period[i].get() != '':
+                date_s = int(dates_from[i].get())
+                period = int(dates_period[i].get())
+                dates.append([date(date_s//10000, (date_s % 10000)//100, date_s % 100), period])
+                total_dates += period
+
+        progress['maximum'] = len(city_list) * total_dates
+        for i in range(len(city_list)):
+            city = city_list[i]
+            cell_row = 3
+            for j in range(len(dates)):
+                date_start, date_period = dates[j]
+                for k in range(date_period):
+                    period = 6 if (city in cityEurope or city in cityAmerica) else 3
+                    date1 = date_start + timedelta(days=k)
+                    date2 = date1 + timedelta(days=period)
+                    prices = get_price(CHROME, city, date1.strftime("%m-%d"), date2.strftime("%m-%d"), airline_list)
+                    sheet.cell(column=2, row=cell_row, value=f'{int(date1.strftime("%Y"))}년 '
+                                                             f'{int(date1.strftime("%m"))}월 '
+                                                             f'{int(date1.strftime("%d"))}일 '
+                                                             f'{day_list[int(date1.strftime("%w"))]}요일')
+                    for l in range(len(airline_list)):
+                        cell_column = 3 + i * len(airline_list) + l
+                        price = prices[l]
+                        if price != -1:
+                            sheet.cell(column=cell_column, row=cell_row, value=prices[l])
+                    workbook.save(f'{today.strftime("%Y%m%d")}.xlsx')
+                    cell_row += 1
+                    progress['value'] += 1
+                    progress.update()
+        workbook.save(f'{today.strftime("%Y%m%d")}.xlsx')
+        CHROME.quit()
+        main.quit()
+
+    pixel = tk.PhotoImage(width=1, height=1)
+    start_btn = tk.Button(main, text='작업 시작', image=pixel, width=192, height=50, compound='c', command=crawling)
+    start_btn.place(x=380, y=300)
+    main.mainloop()
