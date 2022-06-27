7a3dbf482e06deeed22257c8bd01dfdcadfb16b4
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2022-06-27 23:08:28 +0900

    remove unused import

--- crawler.py
+++ crawler.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
